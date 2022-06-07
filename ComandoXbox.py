e1cb0573746a836d41ab75d97c9b746eeb4772f7
henriquejcorreia <101277043+henriquejcorreia@users.noreply.github.com>
2022-06-07 16:39:06 +0100

    Add files via upload

---
+++ ComandoXbox.py
@@ -0,0 +1,49 @@
+from inputs import get_gamepad
+import math
+import threading
+
+
+class XboxController(object):
+    MAX_TRIG_VAL = math.pow(2, 8)
+    MAX_JOY_VAL = math.pow(2, 15)
+
+    def __init__(self):
+
+        self.LeftJoystickY = 0
+        self.LeftJoystickX = 0
+        self.RightJoystickY = 0
+        self.RightJoystickX = 0
+
+        self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
+        self._monitor_thread.daemon = True
+        self._monitor_thread.start()
+
+
+    def read(self):
+        Yaw = self.LeftJoystickX
+        Throttle = self.LeftJoystickY
+        Pitch = self.RightJoystickY
+        Roll = self.RightJoystickX
+
+
+        return [Yaw, Throttle, Pitch, Roll]
+
+
+
+    def _monitor_controller(self):
+        while True:
+            events = get_gamepad()
+            for event in events:
+                if event.code == 'ABS_Y':
+                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_X':
+                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RY':
+                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RX':
+                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+
+if __name__ == '__main__':
+    joy = XboxController()
+    while True:
+        print(joy.read())
