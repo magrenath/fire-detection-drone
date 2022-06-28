30cde611bc5e184366b5cb7e69cb68d016856688
Nathália Magre Guerra Pinto <78081547+magrenath@users.noreply.github.com>
2022-06-28 14:40:26 +0100

    Add files via upload

---
+++ clientGamepad.py
@@ -0,0 +1,54 @@
+import socket
+import pygame
+import time
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('192.168.35.214', 10000)
+print('Connecting to {} port {}'.format(*server_address))
+sock.connect(server_address)
+
+# -----------------------------------------
+# Command Initialization
+# -----------------------------------------
+pygame.init()
+pygame.joystick.init()
+
+# Get count of joysticks.
+joystick_count = pygame.joystick.get_count()
+print('Found ' + str(joystick_count) + ' joystick.')
+
+# init joystick
+joystick = pygame.joystick.Joystick(0)  # Assuming we have only one
+joystick.init()
+
+# Get the name from the OS for the controller/joystick.
+joystick_name = joystick.get_name()
+print('Connected to ' + joystick_name)
+
+# -----------------------------------------
+# GetData
+# -----------------------------------------
+
+while True:
+
+    Y = round(joystick.get_axis(0) * 100)/100
+    T = round(-joystick.get_axis(1) * 100)/100
+    P = round(-joystick.get_axis(3) * 100)/100
+    R = round(joystick.get_axis(2) * 100)/100
+    A = joystick.get_button(0)
+    pygame.event.pump()
+
+    message = str('Y ' + str(Y) + '; T ' + str(T) + '; R ' + str(R) + '; P ' + str(P) + '; A ' + str(A))
+    print(message)
+
+    # -------------------------------------
+    # SendData
+    # -------------------------------------
+
+    byt1 = message.encode()
+    sock.send(byt1)
+
+    time.sleep(0.5)
---
+++ serverGamepad.py
@@ -0,0 +1,20 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('192.168.35.214', 10000)
+print('starting up on {} port {}'.format(*server_address))
+sock.bind(server_address)
+
+# Listen for incoming connections
+sock.listen(1)
+
+while True:
+    print('Drone availiable, waiting connection')
+    connection, client_address = sock.accept()
+    try:
+        print('connection from', client_address)
+        while True:
+            data1 = connection.recv(5000)
+            print(data1.decode())
+    finally:
+        connection.close()
